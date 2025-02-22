18a772cca91e813a78331b3e3b28a26a257be18c
entrepreneurim <iamakashrabha@gmail.com>
2025-02-22 15:35:13 +0530

    Create database.py

---
+++ database/database.py
@@ -0,0 +1,35 @@
+#(©)Javpostr made by @rohit_1888
+
+import time
+import pymongo, os
+import motor, asyncio
+import motor.motor_asyncio
+from config import DB_URI, DB_NAME
+
+class Rohit:
+
+    def __init__(self, DB_URI, DB_NAME):
+        self.dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
+        self.database = self.dbclient[DB_NAME]
+
+        self.user_data = self.database['users']
+
+    # USER MANAGEMENT
+    async def present_user(self, user_id: int):
+        found = await self.user_data.find_one({'_id': user_id})
+        return bool(found)
+
+    async def add_user(self, user_id: int):
+        await self.user_data.insert_one({'_id': user_id})
+        return
+
+    async def full_userbase(self):
+        user_docs = await self.user_data.find().to_list(length=None)
+        user_ids = [doc['_id'] for doc in user_docs]
+        return user_ids
+
+    async def del_user(self, user_id: int):
+        await self.user_data.delete_one({'_id': user_id})
+        return
+
+Rohitdb = Rohit(DB_URI, DB_NAME)
