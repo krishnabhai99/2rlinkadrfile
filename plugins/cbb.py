a7c4e1f389f3683a07a7d3fe2809526ac5ded1ae
entrepreneurim <iamakashrabha@gmail.com>
2025-02-22 18:39:42 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -14,7 +14,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>.</b>",
+            text = f"<b>›› Bʏ: ADR ANIME HINDI</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
