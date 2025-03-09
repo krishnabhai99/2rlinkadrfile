de14fb3ea2dea979bbbed1b1d67de93183f0e570
entrepreneurim <iamakashrabha@gmail.com>
2025-03-09 14:37:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6987158459"))
 #Port
 PORT = os.environ.get("PORT", "8080")
 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://akashrabha2005:781120@cluster0.pv6yd2f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://nextninzagame:Online@123@cluster0.jngj5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "AxomBotz")
 JOIN_REQS_DB = os.environ.get("JOIN_REQS_DB", DB_URI)
 JOIN_REQS_DB2 = os.environ.get("JOIN_REQS_DB2", DB_URI)
